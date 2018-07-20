dd5b19abdaba057cca199952a563ad6a36adda6a
abourke09 <aabourke@gmail.com>
2018-07-20 09:30:08 -0700

    current_user and logged_in? methods added to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= Customer.find(session[:customer_id]) if session[:customer_id]
+  end
+
 end
