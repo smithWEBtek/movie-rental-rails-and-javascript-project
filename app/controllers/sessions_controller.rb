b1788d27e44b77b53632610955ef3f4ae4811229
abourke09 <aabourke@gmail.com>
2018-07-20 09:34:46 -0700

    adds create method to sessions controller with authentication for password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
     @customers = Customer.all
   end
 
-  
+  def create
+    @customer = Customer.find_by(id: params[:customer][:id])
+
+    if @customer && @customer.authenticate(params[:customer][:password])
+      session[:customer_id] = @customer.id
+      redirect_to customer_path(@customer)
+    else
+      redirect_to '/login'
+    end
+  end
+
 
 end
