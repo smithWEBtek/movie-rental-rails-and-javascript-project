efa844f75f741ae1350feebe28baebfe3d93cfa4
abourke09 <aabourke@gmail.com>
2018-07-20 11:44:48 -0700

    authenticaiton route for facebook callback created, comment removed

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :rentals
   resources :movies
   resources :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/facebook/callback' => 'sessions#facebook_create'
+
 end
