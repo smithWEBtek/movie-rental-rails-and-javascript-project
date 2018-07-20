4f28f5ef3ecf87dd6655ba8e7dabaf0ff8ef3290
abourke09 <aabourke@gmail.com>
2018-07-20 09:57:02 -0700

    home method added to ApplicationController to allow for homepage view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   helper_method :current_user
+
+  def home
+    if session[:current_id]
+      @current = Current.find_or_create_by(id: session[:current_id])
+    end
+  end
 
   def logged_in?
     !!current_user
