bf019e62e1ddf4f8a6a0bdf4e41793bd970ce9d7
abourke09 <aabourke@gmail.com>
2018-07-20 09:45:46 -0700

    'create' method added for Customers to be able to sign up.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,5 +6,16 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
-  
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      session[:customer_id] = @customer.id
+      redirect_to customer_path(@customer)
+    else
+      render :new
+    end
+  end
+
+
 end
