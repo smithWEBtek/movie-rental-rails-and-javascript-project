91717ea23803b442a96c9107df745c623b4f9546
abourke09 <aabourke@gmail.com>
2018-07-23 11:00:54 -0700

    edit and update methods added to customers_controller to allow Cusotmer to edit their profile info

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -20,6 +20,20 @@ class CustomersController < ApplicationController
     end
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+
   def require_login
     redirect_to '/' unless logged_in?
   end
