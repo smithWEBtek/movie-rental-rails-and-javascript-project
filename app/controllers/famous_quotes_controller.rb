8806f9b7a560a38b6a5e18820304ccc924b76adc
abourke09 <aabourke@gmail.com>
2018-07-24 16:44:17 -0700

    new, create, and strong params methods added to famous_quotes controller

--- app/controllers/famous_quotes_controller.rb
+++ app/controllers/famous_quotes_controller.rb
@@ -1,2 +1,22 @@
 class FamousQuotesController < ApplicationController
+
+  def new
+    @famous_quote = FamousQuote.new
+  end
+
+  def create
+    @famous_quote = Famous_quote.new(famous_quote_params)
+    if @famous_quote.save
+      @message = "Thank you for adding a quote to #{@famous_quote.movie.title}"
+      redirect_to movie_path(@famous_quote.movie, :message => @message)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def famous_quote_params
+    params.require(:famous_quote).permit(:quote, :actor, :movie_id)
+  end
 end
