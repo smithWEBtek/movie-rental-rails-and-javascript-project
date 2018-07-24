3d71e1f3fde1af54cf093a732f9f424db0a2dc02
abourke09 <aabourke@gmail.com>
2018-07-24 13:19:26 -0700

    logic added to new method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,6 +4,7 @@ class RentalsController < ApplicationController
     @rental = Rental.create(
           :customer_id => params[:customer_id],
           :movie_id => params[:movie_id]
+          
         )
     @message = @rental.rent_movie
     redirect_to customer_path(@rental.customer, :message => @message)
