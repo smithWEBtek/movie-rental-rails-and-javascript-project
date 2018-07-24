271e3a8b78d82cd0c5081b992ced6609ceb53bcf
abourke09 <aabourke@gmail.com>
2018-07-24 15:28:33 -0700

    edits redirect paths and removes notes

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,14 +1,25 @@
 class RentalsController < ApplicationController
 
+  def index
+    if params[:customer_id]
+      @customer = Customer.find_by(id: params[:customer_id])
+      if @customer.nil?
+        redirect_to "/"
+      else
+        @rentals = @customer.rentals
+      end
+    else
+      redirect_to "/"
+    end
+  end
+
   def new
     @rental = Rental.create(
           :customer_id => params[:customer_id],
           :movie_id => params[:movie_id]
-          #change status to rented/returned
         )
     @message = @rental.rent_movie
-    redirect_to customer_path(@rental.customer, :message => @message)
-    #change path url above
+    redirect_to customer_rentals_path(@rental.customer, :message => @message)
   end
 
 end
