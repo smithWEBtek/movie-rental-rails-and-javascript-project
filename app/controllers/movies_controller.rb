4145efcb7bc238f8a64f449afed5ece70af3d2e4
abourke09 <aabourke@gmail.com>
2018-07-24 11:09:04 -0700

    filter logic added to index method in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,28 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    if !params[:rating].blank?
+      if params[:rating] == "G"
+        @movies = Movie.all.select { |m| m.rating == "G" }
+      elsif params[:rating] == "PG"
+        @movies = Movie.all.select { |m| m.rating == "PG" }
+      elsif params[:rating] == "PG-13"
+        @movies = Movie.all.select { |m| m.rating == "PG-13" }
+      elsif params[:rating] == "R"
+        @movies = Movie.all.select { |m| m.rating == "R" }
+      elsif params[:rating] == "NC-17"
+        @movies = Movie.all.select { |m| m.rating == "NC-17" }
+      end
+    else
+      @movies = Movie.all
+    end
+
   end
+
 
   def show
     @movie = Movie.find(params[:id])
   end
 
-  
+
 end
