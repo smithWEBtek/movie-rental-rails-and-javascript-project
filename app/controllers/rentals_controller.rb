a22aeaa86bc28d7e57e62a034474a6d2dac14137
abourke09 <aabourke@gmail.com>
2018-07-24 13:19:00 -0700

    rent_movie method stubbed out with places for messages and if statement for verificaitons. New method added to rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,13 @@
 class RentalsController < ApplicationController
+
+  def new
+    @rental = Rental.create(
+          :customer_id => params[:customer_id],
+          :movie_id => params[:movie_id]
+        )
+    @message = @rental.rent_movie
+    redirect_to customer_path(@rental.customer, :message => @message)
+
+  end
+
 end
