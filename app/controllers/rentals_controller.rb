9d0c92f686174b690151453925ad361192455690
abourke09 <aabourke@gmail.com>
2018-07-24 13:20:12 -0700

    notes

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,11 +4,11 @@ class RentalsController < ApplicationController
     @rental = Rental.create(
           :customer_id => params[:customer_id],
           :movie_id => params[:movie_id]
-          
+          #change status to rented/returned
         )
     @message = @rental.rent_movie
     redirect_to customer_path(@rental.customer, :message => @message)
-
+    #change path url above
   end
 
 end
