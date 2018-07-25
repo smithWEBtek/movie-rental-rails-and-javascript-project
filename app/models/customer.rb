2b15f5a85620805de2af2886c4417c61b68b0dc0
abourke09 <aabourke@gmail.com>
2018-07-25 16:19:42 -0700

    edits made to ensure validation messages are shown

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,8 +2,9 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :movies, through: :rentals
   validates :password_digest, :length => { :minimum => 6}
-  validates :name, uniqueness: true 
-  validates :email, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :age, presence: true, numericality: true
   has_secure_password
 
 end
