26af6642ffd7e376861777806d2c4fcb753e7002
abourke09 <aabourke@gmail.com>
2018-07-25 16:54:16 -0700

    move the movie filter logic from the controller index method to a new model Class method, self.rating_filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,17 +2,7 @@ class MoviesController < ApplicationController
 
   def index
     if !params[:rating].blank?
-      if params[:rating] == "G"
-        @movies = Movie.where("rating = 'G'")
-      elsif params[:rating] == "PG"
-        @movies = Movie.where("rating = 'PG'")
-      elsif params[:rating] == "PG-13"
-        @movies = Movie.where("rating = 'PG-13'")
-      elsif params[:rating] == "R"
-        @movies = Movie.where("rating = 'R'")
-      elsif params[:rating] == "NC-17"
-        @movies = Movie.where("rating = 'NC-17'")
-      end
+      @movies = Movie.rating_filter(params[:rating])
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,19 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
   has_many :famous_quotes
+
+  def self.rating_filter(rating_param)
+    if rating_param == "G"
+      Movie.where("rating = 'G'")
+    elsif rating_param == "PG"
+      Movie.where("rating = 'PG'")
+    elsif rating_param == "PG-13"
+      Movie.where("rating = 'PG-13'")
+    elsif rating_param == "R"
+      Movie.where("rating = 'R'")
+    elsif rating_param == "NC-17"
+      Movie.where("rating = 'NC-17'")
+    end
+  end
+
 end
