cd92b796d3b3af12397a767de167e13cb1df94f8
abourke09 <aabourke@gmail.com>
2018-07-25 11:15:42 -0700

    routes editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
   root 'application#home'
-  resources :rentals
-  resources :movies
-  resources :customers
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :customers
   resources :customers, only: [:show] do
     resources :rentals, only: [:index]
   end
+  get '/signup' => 'customers#new'
+  post '/signup' => 'customers#create'
 
+  post '/rentals/new', to: 'rentals#new'
+  post '/rentals/edit', to: 'rentals#edit'
+
+  resources :movies, only: [:index]
   resources :movies, only: [:show] do
     get '/famous_quotes/new', to: 'famous_quotes#new'
     post '/famous_quotes/new', to: 'famous_quotes#create'
   end
 
-  post '/rentals/new', to: 'rentals#new'
-  post '/rentals/edit', to: 'rentals#edit'
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/auth/facebook/callback' => 'sessions#create'
 
-  get '/logout' => 'sessions#destroy'
 
-  get '/signup' => 'customers#new'
-  post '/signup' => 'customers#create'
+
 
 end
