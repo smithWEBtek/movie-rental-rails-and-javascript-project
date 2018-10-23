c642dc0e761df92c566ab7599a128a0700a72c25
abourke09 <aabourke@gmail.com>
2018-10-23 15:36:34 -0700

    make movie index responsive to .json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,10 @@ class MoviesController < ApplicationController
   def index
     if !params[:rating].blank?
       @movies = Movie.rating_filter(params[:rating])
+      respond_to do |f|
+        f.html
+        f.json {renger json: @movies}
+      end
     else
       @movies = Movie.all
     end
